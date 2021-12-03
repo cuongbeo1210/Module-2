1033ed80e87e5dc9baf81ffee6224fc890aca1ca
cuongkaren <quoccuongd16cn2@gmail.com>
2021-12-03 12:59:07 +0700

    Hiển thị lời chào

---
+++ Introduction to Java/src/hienThiLoiChao.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class hienThiLoiChao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name : ");
+        String name = scanner.nextLine();
+        System.out.printf("Hello : %s", name);
+    }
+}
