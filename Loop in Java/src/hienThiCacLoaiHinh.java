1a36e2699048175fe8e03233adc48365bf5d904f
cuongkaren <quoccuongd16cn2@gmail.com>
2021-12-04 14:20:43 +0700

    Hiển thị các loại hình

--- Loop in Java/src/hienThiCacLoaiHinh.java
+++ Loop in Java/src/hienThiCacLoaiHinh.java
@@ -14,16 +14,16 @@ public class hienThiCacLoaiHinh {
             choice = input.nextInt();
             switch (choice){
                 case 1 :
-                    for(int i = 1; i <= 5; i++){
-                        for(int j = 1; j <= 6; j++) {
+                    for(int i = 1; i <= 7; i++){
+                        for(int j = 1; j <= 10; j++) {
                             System.out.print("*  ");
                         }
                         System.out.println("");
                     }
                     break;
                 case 2 :
-                    for (int i = 1; i <= 6; i++) {
-                        for (int j = 1; j < i; j++) {
+                    for (int i = 1; i <= 7; i++) {
+                        for (int j = 1; j <= i; j++) {
                             System.out.print("*  ");
                         }
                         System.out.println("");
