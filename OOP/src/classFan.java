27a21d442c018882e6f2173264f9b43347aa0c48
cuongkaren <quoccuongd16cn2@gmail.com>
2021-12-07 11:22:56 +0700

    up

---
+++ OOP/src/classFan.java
@@ -0,0 +1,84 @@
+public class classFan {
+    private final int SLOW = 1;
+    private final int MEDIUM = 2;
+    private final int FAST = 3;
+    private int speed = 1;
+
+    public int getSLOW() {
+        return SLOW;
+    }
+
+    public int getMEDIUM() {
+        return MEDIUM;
+    }
+
+    public classFan(int speed, boolean on, String color, double radius) {
+        this.speed = speed;
+        this.on = on;
+        this.color = color;
+        this.radius = radius;
+    }
+
+    public int getFAST() {
+        return FAST;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    private boolean on = false;
+    private String color = "blue";
+    private double radius = 5;
+
+
+
+    @Override
+    public String toString() {
+        String state = "";
+        if (this.isOn()) {
+            state += ("Fan is on\n");
+            state += ("Speed: " + this.speed + "\n");
+        } else {
+            state += ("Fan is off" +"\n");
+        }
+        state += ("Color: " + this.color + "\n");
+        state += ("Radius: " + this.radius + "\n");
+        return state;
+    }
+    public static void main(String[] args) {
+        classFan fan1 =new classFan(3,true,"yellow",10);
+        classFan fan2 =new classFan(2,false,"blue",5);
+        System.out.println(fan1.toString());
+        System.out.println(fan2.toString());
+    }
+
+}
