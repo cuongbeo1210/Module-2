892b50b09075840ca012a69569334f89159ef002
cuongkaren <quoccuongd16cn2@gmail.com>
2021-12-10 01:23:10 +0700

    Up thiếu .

---
+++ Inheritance/src/testArray/MainProduct.java
@@ -0,0 +1,106 @@
+package testArray;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MainProduct {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Product> products = new ArrayList<Product>();
+        enterProduct(scanner, products);
+        sumPrice(products);
+        findProduct(scanner, products);
+        editProduct(scanner, products);
+        deleteProduct(scanner, products);
+
+        System.out.println(products);
+    }
+
+
+    public static Product createProduct(Scanner scanner) {
+        System.out.print("Enter ID : ");
+        int id = scanner.nextInt();
+        System.out.print("Enter Price : ");
+        double price = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.print("Enter Name : ");
+        String name = scanner.nextLine();
+        System.out.print("Enter Type : ");
+        String type = scanner.nextLine();
+        System.out.print("Enter Money : ");
+        String money = scanner.nextLine();
+        return new Product(id, name, price, type, money);
+    }
+
+    public static void deleteProduct(Scanner scanner, ArrayList<Product> products) {
+        System.out.print("Nhập ID muốn xóa : ");
+        boolean check = false;
+        int id = scanner.nextInt();
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getID() == id) {
+                products.remove(i);
+                check = true;
+                break;
+            }
+        }
+        if (!check) {
+            System.out.println("ID này không trùng sản phẩm");
+        }
+    }
+
+    public static void findProduct(Scanner scanner, ArrayList<Product> products) {
+        boolean check = false;
+        System.out.print("Nhập ID sản phẩm muốn tìm : ");
+        int id = scanner.nextInt();
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getID() == id) {
+                check = true;
+                System.out.println(products.get(i));
+                break;
+            }
+        }
+        if (!check) {
+            System.out.println("Không có sản phẩm muốn tìm");
+        }
+    }
+
+    public static void enterProduct(Scanner scanner, ArrayList<Product> products) {
+        System.out.print("Nhập số sản phẩm : ");
+        int number = scanner.nextInt();
+        for (int i = 0; i < number; i++) {
+            products.add(createProduct(scanner));
+        }
+        System.out.println(products);
+    }
+
+    public static void editProduct(Scanner scanner, ArrayList<Product> products){
+        boolean check = false;
+        System.out.print("Nhập ID sản phẩm muốn sửa : ");
+        int id = scanner.nextInt();
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getID() == id) {
+                check = true;
+                System.out.print("Nhập giá mới : ");
+                products.get(i).setPrice(scanner.nextDouble());
+                System.out.print("Nhập tên mới : ");
+                scanner.nextLine();
+                products.get(i).setName(scanner.nextLine());
+                System.out.print("Nhập kiểu mới : ");
+                products.get(i).setType(scanner.nextLine());
+                System.out.println(products.get(i));
+                break;
+            }
+        }
+        if (!check){
+            System.out.println("ID không tồn tại");
+        }
+    }
+
+    public static void sumPrice(ArrayList<Product> products){
+        double sum = 0;
+        for(Product product : products){
+            sum += product.getPrice();
+        }
+        System.out.println("Tổng giá tiền là : "+sum);
+    }
+}
---
+++ Inheritance/src/testArray/Product.java
@@ -0,0 +1,71 @@
+package testArray;
+
+public class Product {
+    private int ID;
+    private String name;
+    private double price;
+    private String type;
+    private String money;
+
+    public Product() {
+    }
+
+    public Product(int ID, String name, double price, String type, String money) {
+        this.ID = ID;
+        this.name = name;
+        this.price = price;
+        this.type = type;
+        this.money = money;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "ID=" + ID +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", type='" + type + '\'' +
+                ", money='" + money + '\'' +
+                '}';
+    }
+
+    public String getMoney() {
+        return money;
+    }
+
+    public void setMoney (String money) {
+        this.money = money;
+    }
+}
