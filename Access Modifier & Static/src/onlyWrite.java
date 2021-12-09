8a0a10a29a480fa3391af26b01bb92311dce89f2
cuongkaren <quoccuongd16cn2@gmail.com>
2021-12-09 09:30:31 +0700

    update

--- Access Modifier & Static/src/onlyWrite.java
+++ Access Modifier & Static/src/onlyWrite.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class onlyWrite {
     private String name = "Cuong";
     private String Class = "C1021H1";
@@ -23,9 +25,19 @@ public class onlyWrite {
 
     public static void main(String[] args) {
         onlyWrite ow = new onlyWrite();
+        onlyWrite ow1 = new onlyWrite();
         System.out.println(ow);
         ow.setName("Beo");
         ow.setClass("CO2");
         System.out.println(ow);
+        Input(ow1);
+        System.out.println(ow1);
+    }
+    public static void Input(onlyWrite ow){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name : ");
+        ow.setName(scanner.nextLine());
+        System.out.print("Enter your class : ");
+        ow.setClass(scanner.nextLine());
     }
 }
