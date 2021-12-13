dad89dc953620a36511f02171e89a10549231a31
cuongkaren <quoccuongd16cn2@gmail.com>
2021-12-13 16:09:42 +0700

    interface

---
+++ Abstract Class & Interface/src/comparableCircle/Circle.java
@@ -0,0 +1,56 @@
+package comparableCircle;
+
+public class Circle {
+    private double radius = 1.1;
+    private String color = "red";
+    private boolean filled = true;
+
+
+    public Circle() {
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        this.radius = radius;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+}
