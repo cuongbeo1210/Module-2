dfdb1c3524fd315a29fe0a48b4a20614148105be
cuongkaren <quoccuongd16cn2@gmail.com>
2021-12-17 11:43:17 +0700

    Stack

---
+++ Stack & Queue/src/thuchanh/reversebystack/Stack.java
@@ -0,0 +1,45 @@
+package thuchanh.reversebystack;
+
+public class Stack {
+    int size;
+    int top;
+    char[] a;
+
+    boolean isEmpty()
+    {
+        return (top < 0);
+    }
+
+    Stack(int n)
+    {
+        top = -1;
+        size = n;
+        a = new char[size];
+    }
+
+    void push(char x)
+    {
+        if (top >= size)
+        {
+            System.out.println("Stack Overflow");
+        }
+        else
+        {
+            a[++top] = x;
+        }
+    }
+
+    char pop()
+    {
+        if (top < 0)
+        {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        else
+        {
+            return a[top--];
+        }
+    }
+}
+
---
+++ Stack & Queue/src/thuchanh/reversebystack/reverseByStack.java
@@ -0,0 +1,35 @@
+package thuchanh.reversebystack;
+
+public class reverseByStack {
+
+        public static void reverse(StringBuffer str)
+        {
+
+            int n = str.length();
+            Stack obj = new Stack(n);
+
+
+            int i;
+            for (i = 0; i < n; i++)
+                obj.push(str.charAt(i));
+
+
+            for (i = 0; i < n; i++)
+            {
+                char ch = obj.pop();
+                str.setCharAt(i,ch);
+            }
+        }
+
+
+        public static void main(String[] args)
+        {
+
+            StringBuffer s= new StringBuffer("Cuongdeptrai");
+
+            reverse(s);
+
+            System.out.println("Reversed string is " + s);
+        }
+    }
+
